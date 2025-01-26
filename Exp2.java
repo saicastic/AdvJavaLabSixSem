9d8a3c9021e41faf6a7e21011424379a50db06b9
saicastic <saiprithvisuperior@gmail.com>
2025-01-26 23:35:15 +0530

    added Exp13.java

--- Exp2.java
+++ Exp2.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class Exp2 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("How");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Set the size of the frame
         frame.setSize(400, 400);
@@ -13,8 +14,6 @@ public class Exp2 {
 
         // Make the frame visible
         frame.setVisible(true);
-
-        // Exit the program when the frame is closed
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
+
