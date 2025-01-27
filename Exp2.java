f10c838abc5c668d0d209e5cb69d1f99f20ef0f8
saicastic <saiprithvisuperior@gmail.com>
2025-01-27 20:45:15 +0530

    modified Exp2.java

--- Exp2.java
+++ Exp2.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Exp2 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("How");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Hello World");
+
+        frame.add(label);
 
         // Set the size of the frame
         frame.setSize(400, 400);
---
+++ Exp3.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Exp3 {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
